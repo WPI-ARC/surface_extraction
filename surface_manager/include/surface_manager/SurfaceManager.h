f0c50a7adae68d19ef64872b161c39249a542e20
Will Pryor <jwpryor@wpi.edu>
2016-03-02 03:03:29 -0500

    Fixed ExpandSurfaces
    - Put inliers back into the Surfaces message
    - SurfaceManager still doesn't listen to replace_surfaces messages

--- surface_manager/include/surface_manager/SurfaceManager.h
+++ surface_manager/include/surface_manager/SurfaceManager.h
@@ -9,9 +9,13 @@
 #include <nodelet/nodelet.h>
 
 #include <pcl/point_types.h>
+#include <pcl/geometry/triangle_mesh.h>
+#include <pcl/geometry/mesh_conversion.h>
 #include <pcl_conversions/pcl_conversions.h>
 #include <pcl_ros/point_cloud.h>
 #include <ros/ros.h>
+#include <sensor_msgs/point_cloud2_iterator.h>
+#include <sensor_msgs/point_cloud_conversion.h>
 #include <pcl/PointIndices.h>
 #include <surfaces/PointIndices_Serialization.hpp>
 #include <pcl/ModelCoefficients.h>
@@ -20,23 +24,47 @@
 #include <message_filters/synchronizer.h>
 #include <message_filters/sync_policies/exact_time.h>
 #include <message_filters/sync_policies/approximate_time.h>
-#include <surfaces/Polygons.hpp>
-#include <surfaces/Polygons_Serialization.hpp>
-#include <surfaces/Vertices_Serialization.hpp>
-#include <surfaces/Surface.hpp>
-#include <surfaces/Surfaces.hpp>
-#include <surfaces/Surfaces_Serialization.hpp>
+//#include <surfaces/Polygons.hpp>
+//#include <surfaces/Polygons_Serialization.hpp>
+//#include <surfaces/Vertices_Serialization.hpp>
+//#include <surfaces/Surface.hpp>
+//#include <surfaces/Surfaces.hpp>
+//#include <surfaces/Surfaces_Serialization.hpp>
+#include <sensor_msgs/PointCloud2.h>
+#include <surface_msgs/Surface.h>
+#include <surface_msgs/Surfaces.h>
+#include <surface_msgs/SurfaceMesh.h>
+#include <surface_msgs/SurfaceMeshes.h>
+#include <surface_msgs/SurfaceCloud.h>
+#include <surface_msgs/SurfaceClouds.h>
 
 namespace surface_manager {
     class SurfaceManager : public nodelet::Nodelet {
-        typedef pcl::PointXYZ PointIn;
-        typedef pcl::PointCloud<PointIn> PointCloudIn;
+//        typedef pcl::PointXYZ PointIn;
+        typedef sensor_msgs::PointCloud2 PointCloudIn;
+
 
         typedef pcl_msgs::PolygonMesh PolygonMesh;
         typedef pcl_msgs::ModelCoefficients ModelCoefficients;
 
         typedef surface_msgs::Surface Surface;
         typedef surface_msgs::Surfaces Surfaces;
+
+//        typedef surface_msgs::SurfaceCloud SurfaceCloud;
+//        typedef surface_msgs::SurfaceClouds SurfaceClouds;
+
+        typedef surface_msgs::SurfaceMesh SurfaceMesh;
+        typedef surface_msgs::SurfaceMeshes SurfaceMeshes;
+
+        template <bool IsManifoldT>
+        struct MeshTraits
+        {
+            typedef int                                          VertexData;
+            typedef pcl::geometry::NoData                        HalfEdgeData;
+            typedef pcl::geometry::NoData                        EdgeData;
+            typedef pcl::geometry::NoData                        FaceData;
+            typedef boost::integral_constant <bool, IsManifoldT> IsManifold;
+        };
 
         template<typename ...S>
         using ExactTimeSynchronizer = message_filters::Synchronizer<message_filters::sync_policies::ExactTime<S...> >;
@@ -47,6 +75,9 @@ namespace surface_manager {
 
     private:
         ros::Publisher surfaces_pub_;
+//        ros::Publisher surface_clouds_pub_;
+        ros::Publisher surface_meshes_pub_;
+        ros::Publisher output_pub_;
 
         message_filters::Subscriber<PointCloudIn>  new_surface_inliers_sub_;
         message_filters::Subscriber<PolygonMesh> new_surface_convex_hull_sub_;
@@ -55,13 +86,18 @@ namespace surface_manager {
         boost::shared_ptr<NewSurfaceSynchronizer> new_surface_synchronizer_;
 
         std::vector<Surface> surfaces;
-//        std::vector<surfa
+//        std::vector<SurfaceCloud> surface_clouds;
+        std::vector<SurfaceMesh> surface_meshes;
 
-        void add_surface_synchronized(const pcl::PointCloud<pcl::PointXYZ>::ConstPtr inliers,
+        void add_surface_synchronized(const PointCloudIn::ConstPtr inliers,
                                       const PolygonMesh::ConstPtr concave_hull,
                                       const ModelCoefficients::ConstPtr model);
+
+        void publish(std_msgs::Header header);
+
     };
 }
 
 
 #endif //SURFACE_MANAGER_SURFACEMANAGER_H
+typedef pcl::PointCloud<pcl::PointXYZRGB> PointCloudOut;
--- surface_manager/src/surface_manager/SurfaceManager.cpp
+++ surface_manager/src/surface_manager/SurfaceManager.cpp
@@ -20,6 +20,9 @@ void surface_manager::SurfaceManager::onInit() {
 
     // Advertise a topic called "surfaces" with a queue size of max_queue_size and latched=true
     surfaces_pub_ = private_nh.advertise<Surfaces>("surfaces", (uint32_t) max_queue_size, true);
+//    surface_clouds_pub_ = private_nh.advertise<SurfaceClouds>("surface_clouds", (uint32_t) max_queue_size, true);
+    surface_meshes_pub_ = private_nh.advertise<SurfaceMeshes>("surface_meshes", (uint32_t) max_queue_size, true);
+    output_pub_ = private_nh.advertise<PointCloudOut>("output", (uint32_t) max_queue_size);
 
     // Subscribe to new_surface_* inputs (for adding surfaces)
     new_surface_inliers_sub_.subscribe(private_nh, INLIERS_TOPIC, (uint32_t) max_queue_size);
@@ -41,13 +44,16 @@ void surface_manager::SurfaceManager::onInit() {
 void surface_manager::SurfaceManager::add_surface_synchronized(const PointCloudIn::ConstPtr inliers,
                                                                const PolygonMesh::ConstPtr concave_hull,
                                                                const ModelCoefficients::ConstPtr model) {
-    auto cloud_size = concave_hull->cloud.width * concave_hull->cloud.height;
-    NODELET_DEBUG("New Surface received with %lu points, %u vertices", inliers->size(), cloud_size);
+    auto cloud_size = inliers->width * inliers->height;
+    auto hull_size = concave_hull->cloud.width * concave_hull->cloud.height;
+    NODELET_DEBUG("New Surface received with %u points, %u vertices", cloud_size, hull_size);
 
     unsigned long id = surfaces.size();
     surfaces.resize(id+1);
+//    surface_clouds.resize(id+1);
+    surface_meshes.resize(id+1);
 
-    surfaces[id].id = (unsigned int) id;
+    surfaces[id].id = static_cast<unsigned int>(id);
     surfaces[id].color = std_msgs::ColorRGBA();
     surfaces[id].color.r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
     surfaces[id].color.g = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
@@ -56,20 +62,77 @@ void surface_manager::SurfaceManager::add_surface_synchronized(const PointCloudI
     surfaces[id].model = *model;
     surfaces[id].concave_hull = *concave_hull;
 
-//    surfaces[id].inliers = *inliers;
+//    surface_clouds[id].id = static_cast<unsigned int>(id);
+    surfaces[id].inliers = *inliers;
 
-    std::sort(surfaces.begin(), surfaces.end(), [](const Surface& a, const Surface& b) {
-        auto a_size = a.concave_hull.cloud.width * a.concave_hull.cloud.height;
-        auto b_size = b.concave_hull.cloud.width * b.concave_hull.cloud.height;
-        // A should come first (compare lower) if its size is greater than B's
-        return a_size > b_size;
-    });
 
-    Surfaces::Ptr surfaces_msg = boost::make_shared<Surfaces>();
-    surfaces_msg->header = concave_hull->header;
-    surfaces_msg->surfaces = surfaces;
+    surface_meshes[id].id = static_cast<unsigned int>(id);
+    // Get triangle mesh
 
-    surfaces_pub_.publish(surfaces_msg);
+//    NODELET_DEBUG_STREAM("[" << getName() << ":add_surface_synchronized] Triangle mesh finished with" << left_out <<
+//                         "vertices unable to be converted");
+
+
+//    std::sort(surfaces.begin(), surfaces.end(), [](const Surface& a, const Surface& b) {
+//        auto a_size = a.concave_hull.cloud.width * a.concave_hull.cloud.height;
+//        auto b_size = b.concave_hull.cloud.width * b.concave_hull.cloud.height;
+//        // A should come first (compare lower) if its size is greater than B's
+//        return a_size > b_size;
+//    });
+
+    this->publish(concave_hull->header);
 }
+
+void surface_manager::SurfaceManager::publish(std_msgs::Header header) {
+    if (surfaces_pub_.getNumSubscribers() > 0) {
+        Surfaces::Ptr surfaces_msg = boost::make_shared<Surfaces>();
+        surfaces_msg->header = header;
+        surfaces_msg->surfaces = surfaces;
+        surfaces_pub_.publish(surfaces_msg);
+    }
+
+//    if (surface_clouds_pub_.getNumSubscribers() > 0) {
+//        SurfaceClouds::Ptr surface_clouds_msg = boost::make_shared<SurfaceClouds>();
+//        surface_clouds_msg->header = header;
+//        surface_clouds_msg->surfaces = surface_clouds;
+//        surface_clouds_pub_.publish(surface_clouds_msg);
+//    }
+
+    if (surface_meshes_pub_.getNumSubscribers() > 0) {
+        SurfaceMeshes::Ptr surface_meshes_msg = boost::make_shared<SurfaceMeshes>();
+        surface_meshes_msg->header = header;
+        surface_meshes_msg->surfaces = surface_meshes;
+        surface_meshes_pub_.publish(surface_meshes_msg);
+    }
+
+    if (output_pub_.getNumSubscribers() > 0) {
+        PointCloudOut::Ptr output = boost::make_shared<PointCloudOut>();
+
+        for (Surface surface : surfaces) {
+            size_t outputPointsSize = output->points.size();
+            size_t projectedPointsSize = surface.inliers.height * surface.inliers.width;
+            output->points.resize(outputPointsSize + projectedPointsSize);
+
+            // C++ makes me sad
+            sensor_msgs::PointCloud2Iterator<float> iter_x(surface.inliers, "x");
+            sensor_msgs::PointCloud2Iterator<float> iter_y(surface.inliers, "y");
+            sensor_msgs::PointCloud2Iterator<float> iter_z(surface.inliers, "z");
+
+            for (int i = 0; iter_x != iter_x.end(); ++iter_x, ++iter_y, ++iter_z, ++i) {
+                output->points[outputPointsSize + i].x = *iter_x;
+                output->points[outputPointsSize + i].y = *iter_y;
+                output->points[outputPointsSize + i].z = *iter_z;
+                output->points[outputPointsSize + i].r = static_cast<uint8_t>(surface.color.r * 255);
+                output->points[outputPointsSize + i].g = static_cast<uint8_t>(surface.color.g * 255);
+                output->points[outputPointsSize + i].b = static_cast<uint8_t>(surface.color.b * 255);
+            }
+        }
+
+        pcl_conversions::toPCL(header, output->header);
+        output_pub_.publish(output);
+    }
+
+}
+
 
 PLUGINLIB_EXPORT_CLASS(surface_manager::SurfaceManager, nodelet::Nodelet)
